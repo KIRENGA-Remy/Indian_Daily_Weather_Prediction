c5035d19afe354c383a29ac42b39c179d864ca2c
GITOLI Remy Claudien <gitoliremy@gmail.com>
2025-03-31 20:03:13 -0700

    Url modifications

--- backend/weather_prediction/weather_app/urls.py
+++ backend/weather_prediction/weather_app/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from .views import get_weather_prediction
 
-print("Weather app URLs loaded")  # Confirm this file is loaded
-
 urlpatterns = [
     path('predict/', get_weather_prediction, name='predict_weather'),
 ]
-
-print("Weather app URL patterns:", urlpatterns)  # Show the registered patterns
